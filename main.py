b239f965d9e07c9e6ace8a2a6adaddb3afb9c74a
igna <igna@pomf.club>
2023-06-06 01:01:23 +0000

    Update main.py

--- main.py
+++ main.py
@@ -3,73 +3,62 @@ import discord
 import orjson
 from helpers.logger import Logger
 
-bot = discord.Client(
-    self_bot=True,
-    chunk_guilds_at_startup=False,
-    max_messages=None,
-    request_guilds=False,
-)
+with open('config.json', 'rb') as f:
+    config = orjson.loads(f.read())
 
 log = Logger('purger')
 
-with open('config.json', 'rb') as f:
-    config = orjson.loads(f.read())
+blacklist = config.get('blacklist', [])
+
+bot = discord.Client(
+    self_bot=True,
+    chunk_guilds_at_startup=False,
+    max_messages=1,
+    request_guilds=False,
+)
 
 
 @bot.event
 async def on_ready():
-    log.info(
-        f'Online | {bot.user.name}#{bot.user.discriminator} ({bot.user.id})'
-    )
-
+    log.info(f'Online | {bot.user.name}#{bot.user.discriminator} ({bot.user.id})')
+    log.info(f'Blacklist | {len(blacklist)} users/channels') 
 
 @bot.event
 async def on_message(message):
-    if message.author.id != bot.user.id:
+    if message.author != bot.user:
         return
 
     if message.content == config['message']:
         async for msg in message.channel.history(limit=None):
-            if msg.author.id == bot.user.id:
+            if msg.author == bot.user:
                 try:
                     await msg.delete()
-                    log.success(
-                        f'Deleted | {msg.content} ({msg.id}) @ {get_channel_name(msg.channel)} ({msg.channel.id})'
-                    )
+                    log.success(f'Deleted | {msg.content} ({msg.id}) @ {get_channel_name(msg.channel)} ({msg.channel.id})')
                 except Exception:
-                    log.error(
-                        f'Failed to delete | {msg.content} ({msg.id}) @ {get_channel_name(msg.channel)} ({msg.channel.id})'
-                    )
+                    log.error(f'Failed to delete | {msg.content} ({msg.id}) @ {get_channel_name(msg.channel)} ({msg.channel.id})')
+
+    if message.author.id in blacklist or message.channel.id in blacklist:
+        return
 
     await asyncio.sleep(300)
     try:
         await message.delete()
+        log.success(f'Deleted | {message.content} ({message.id}) @ {get_channel_name(message.channel)} ({message.channel.id})')
         await asyncio.sleep(5)
-        log.success(
-            f'Deleted | {message.content} ({message.id}) @ {get_channel_name(message.channel)} ({message.channel.id})'
-        )
     except Exception:
-        log.error(
-            f'Failed to delete | {message.content} ({message.id}) @ {get_channel_name(message.channel)} ({message.channel.id})'
-        )
+        log.error(f'Failed to delete | {message.content} ({message.id}) @ {get_channel_name(message.channel)} ({message.channel.id})')
         await asyncio.sleep(10)
         try:
             await message.delete()
-            log.success(
-                f'Deleted | {message.content} ({message.id}) @ {get_channel_name(message.channel)} ({message.channel.id})'
-            )
+            log.success(f'Deleted | {message.content} ({message.id}) @ {get_channel_name(message.channel)} ({message.channel.id})')
         except Exception:
-            log.error(
-                f'Failed to delete again | {message.content} ({message.id}) @ {get_channel_name(message.channel)} ({message.channel.id})'
-            )
+            log.error(f'Failed to delete again | {message.content} ({message.id}) @ {get_channel_name(message.channel)} ({message.channel.id})')
 
 
 def get_channel_name(channel):
     if isinstance(channel, discord.DMChannel):
         return f'{channel.recipient.name}#{channel.recipient.discriminator}'
-    elif isinstance(channel, discord.GroupChannel):
-        return channel.name
-    elif isinstance(channel, discord.TextChannel):
+    elif isinstance(channel, (discord.GroupChannel, discord.TextChannel)):
         return channel.name
     else:
         return 'Unknown'
