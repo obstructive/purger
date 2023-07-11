5dff0c1de8221c336959e5e5c4c843abef6ace83
igna <igna@pomf.club>
2023-07-11 10:27:25 -0400

    Update logger.py

--- helpers/logger.py
+++ helpers/logger.py
@@ -2,22 +2,19 @@ from colorama import Fore, Back, Style, init
 
 init(autoreset=True, convert=True)
 
-
 class Logger:
     def __init__(self, name):
         self.name = name
 
+    def log(self, message, prefix, fore_color, back_color):
+        formatted_message = f'{back_color}{fore_color}{prefix}{Style.RESET_ALL} {message}'
+        print(formatted_message)
+
     def success(self, message):
-        self._print_message(message, Fore.WHITE, Back.GREEN, '[+]', Fore.WHITE)
+        self.log(message, '[+]', Fore.WHITE, Back.GREEN)
 
     def error(self, message):
-        self._print_message(message, Fore.WHITE, Back.RED, '[-]', Fore.WHITE)
+        self.log(message, '[-]', Fore.WHITE, Back.RED)
 
     def info(self, message):
-        self._print_message(message, Fore.WHITE, Back.BLUE, '[~]', Fore.WHITE)
-
-    def _print_message(
-        self, message, fore_color, back_color, prefix, reset_color
-    ):
-        formatted_message = f'{back_color}{fore_color}{prefix}{Style.RESET_ALL} {message}{reset_color}'
-        print(formatted_message)
+        self.log(message, '[~]', Fore.WHITE, Back.BLUE)
